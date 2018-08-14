36d46db7d1bb5a0fd26ae9c131bed794290fe92b
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 13:52:22 +0200

    form added

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,12 @@
 <template>
 <section class="container">
-  <div>
-    contact
+  <div class="wrap">
+    <form class="" action="" method="post" @submit.prevent>
+      <input type="text" name="" value="" placeholder="Name" required>
+      <input type="email" name="" value="" placeholder="Email" required>
+      <textarea class="message" name="name" rows="5" placeholder="Message"></textarea>
+      <input class="button" type="submit" name="" value="Send">
+    </form>
   </div>
 </section>
 </template>
