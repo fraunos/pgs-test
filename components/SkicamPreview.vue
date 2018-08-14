45da66e26649c553eaacd72478700183169eba44
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 10:25:57 +0200

    fetch skicams and display in a component

---
+++ components/SkicamPreview.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="skicam">
+  {{skicam.name}}
+  <img :src="currentImage" alt="">
+</div>
+</template>
+<script>
+export default {
+  props: ['skicam'],
+  created() {
+    this.skicam.cams = Object.keys(this.skicam.cams).map(key => this.skicam.cams[key])
+    setInterval(function () {
+      this.counter++
+    }, 500)
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  computed: {
+    currentImage() {
+      return this.skicam.cams[this.counter % this.skicam.cams.length].url
+    }
+  }
+}
+</script>
+
+<style>
+img {
+  height: 200px;
+  width: 200px;
+}
+</style>
--- pages/skicams.vue
+++ pages/skicams.vue
@@ -1,18 +1,39 @@
 <template>
 <section class="container">
-  <div>
-    skicams
+  <div v-if="skicamsFiltered">
+    <skicam-preview v-for="(skicam, index) in skicamsFiltered" :key="index" :skicam="skicam"></skicam-preview>
   </div>
 </section>
 </template>
 
 <script>
+import SkicamPreview from '~/components/SkicamPreview.vue'
+
 export default {
-  components: {},
+  components: {
+    SkicamPreview
+  },
+  mounted() {
+    this.fetchSkicams()
+  },
+  data() {
+    return {
+      skicams: []
+    }
+  },
+  computed: {
+    skicamsFiltered () {
+      return this.skicams.filter((item) => item.name === "Andalo" || item.name === "Monte Bondone" ? true : false)
+    }
+  },
   methods: {
-    async fetchSomething() {
-      const ip = await this.$axios.$get('http://icanhazip.com')
-      this.ip = ip
+    async fetchSkicams() {
+      const skicams = await this.$axios.$get('https://makevoid-skicams.p.mashape.com/cams.json', {
+        headers: {
+          'X-Mashape-Key': 'kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw'
+        }
+      })
+      this.skicams = Object.keys(skicams).map(key => skicams[key])
     }
   }
 }
