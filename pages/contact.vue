d50e001ffb2c1a93d0609a991dc3c2b2718c4f3b
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 09:35:26 +0200

    init routes

---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+<section class="container">
+  <div>
+    contact
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/skicams.vue
@@ -0,0 +1,17 @@
+<template>
+<section class="container">
+  <div>
+    skicams
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
+
+<style>
+
+</style>
