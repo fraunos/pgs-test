a1c68fb7fa5fe7bd44e1b681887456025234a472
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 09:35:48 +0200

    add nav

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="">
-    header
-  </div>
+  <header class="">
+    <div class="logo">
+      LOGO
+    </div>
+    <nav>
+      <nuxt-link v-for="(route, index) in routes" :key="index" :to="route.url">{{route.name}}</nuxt-link>
+    </nav>
+  </header>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      routes: [
+        {
+          name: 'About us',
+          url: '/'
+        },
+        {
+          name: 'Skicams',
+          url: '/skicams'
+        },
+        {
+          name: 'Contact',
+          url: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 </style>
