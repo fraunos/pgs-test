f15e5cc1fd655154a474ff89dc957bb0fb4d1d03
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 09:36:00 +0200

    add ajax lib - axios

--- pages/skicams.vue
+++ pages/skicams.vue
@@ -9,6 +9,12 @@
 <script>
 export default {
   components: {},
+  methods: {
+    async fetchSomething() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      this.ip = ip
+    }
+  }
 }
 </script>
 
