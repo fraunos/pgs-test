375cbd6205da869641f4ad95ae29a2afc11e643b
Michał Wołoszyn <fraunos@gmail.com>
2018-08-14 13:51:11 +0200

    about us cards rendering

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="container">
-    <div>
-      about us
+    <div class="cards">
+      <div v-for="n in 3" :key="n" class="card">
+        <div class="img">
+        </div>
+        <div class="desc">
+          <h1>{{lorem.head}}</h1>
+          <p>{{lorem.body}}</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -9,6 +16,14 @@
 <script>
 export default {
   components: {
+  },
+  data () {
+    return {
+      lorem: {
+        head: 'Lorem ipsum',
+        body: 'Etiam ullamcorper. Suspendisse a pellentesque dui, non felis maecenas.'
+      }
+    }
   }
 }
 </script>
